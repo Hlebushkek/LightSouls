97ce7b33c23dc0bdbad669c40455f5878a97d5ef
FallWaterFall <volsoor@gmail.com>
2022-04-01 22:38:20 +0300

    init

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LS
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        PlayerControls inputAction;
+        CameraHandler cameraHandler;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        private void Awake()
+        {
+            cameraHandler = CameraHandler.singleton;
+        }
+        private void FixedUpdate()
+        {
+            float delta = Time.deltaTime;
+
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, mouseX, mouseY);
+            }
+        }
+        public void OnEnable()
+        {
+            if (inputAction == null)
+            {
+                inputAction = new PlayerControls();
+                inputAction.PlayerMovement.Movement.performed += inputAction => movementInput = inputAction.ReadValue<Vector2>();
+                inputAction.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+
+            inputAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputAction.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+    }
+}
